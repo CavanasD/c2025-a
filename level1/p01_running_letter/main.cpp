8316a858e082bf6a8f4fd930bd770dcd3a239a02
CavanasD <m18404288789@163.com>
2025-09-01 16:14:16 +0800

    修复了，但感觉效果一般

--- level1/p01_running_letter/main.cpp
+++ level1/p01_running_letter/main.cpp
@@ -4,17 +4,18 @@
 
 int main() {
     std::string word = "A";
-    int width = 80;
+    int width = 60;
+    int MaxRightW = width - (int)word.size();
     int pos = 0;
     int dir = 1;
-    int right = width - (int)word.size();
 
     while (true) {
-        std::cout << "\r";               // 回到行首
-        std::cout << std::string(pos, ' ') << word << std::flush;
-        std::this_thread::sleep_for(std::chrono::milliseconds(60));
+        std::cout << "\r" << std::string(width, ' ');
+        std::cout << "\r" << std::string(pos, ' ') << word << std::flush;
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
         pos += dir;
-        if (pos >= right) { pos = right; dir = -1; }
+        if (pos >= MaxRightW) { pos = MaxRightW; dir = -1; }
         else if (pos <= 0) { pos = 0; dir = 1; }
     }
 }
